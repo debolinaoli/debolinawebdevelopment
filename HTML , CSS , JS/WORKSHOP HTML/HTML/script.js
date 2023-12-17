feaf378a2af1cb231868f26a3d4d3465999d82d0
DEBOLINA <debolinaoli10@gmail.com>
2023-12-17 23:37:32 +0530

    uploading html,css,javascript file

---
+++ HTML , CSS , JS/WORKSHOP HTML/HTML/script.js
@@ -0,0 +1,45 @@
+var a=10;
+let b=10; 
+// it can't be redeclared as i have used let 
+// const d=20
+// through out the program th evalue will be same
+var c;
+c=a+b;
+console.log(c);
+
+function add(){
+    var a=document.getElementById("number1").value; 
+    var b=document.getElementById("number2").value;
+
+    var c=parseInt(a)+parseInt(b);
+    document.getElementById("answer").value=c;
+}
+
+function sub(){
+    var a=document.getElementById("number1").value;
+    var b=document.getElementById("number2").value;
+
+    var c=parseInt(a)-parseInt(b);
+    document.getElementById("answer").value=c;
+
+
+
+}
+function div(){
+    var a=document.getElementById("number1").value;
+    var b=document.getElementById("number2").value;
+
+    var c=parseInt(a)/parseInt(b);
+    document.getElementById("answer").value=c;
+
+}
+
+function mul(){
+    var a=document.getElementById("number1").value;
+    var b=document.getElementById("number2").value;
+
+    var c=parseInt(a)*parseInt(b);
+    document.getElementById("answer").value=c;
+
+}
+ 
---
+++ HTML , CSS , JS/WORKSHOP HTML/script/script1.js
@@ -0,0 +1,18 @@
+function equation(val){
+    document.getElementById("txt").value+=val;
+
+}
+
+
+function calculation(){
+
+    var a =document.getElementById("txt").value-
+    var c = eval()
+}
+
+
+function clear(val){
+    document.getElementById("txt").value="";
+
+
+}
